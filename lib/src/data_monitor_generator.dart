492bd2aa9ba6a8ad72dae6d231e56e92b53ed83c
Loz Groves <lgroves@nqcomputing.com>
2026-06-09 20:20:36 +0300

    Temporary error free

--- lib/src/data_monitor_generator.dart
+++ lib/src/data_monitor_generator.dart
@@ -3,36 +3,36 @@ import 'package:analyzer/dart/element/type.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
-import 'package:data_monitor/annotations.dart';
-import 'package:data_monitor_generators/generators.dart';
-
-class DataMonitorGenerator extends GeneratorForAnnotation<DataMonitor> {
-  @override
-  String generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
-    final ModelVisitor visitor = ModelVisitor();
-    final StringBuffer buffer  = StringBuffer();
-
-    element.visitChildren(visitor);
-
-    buffer.writeln('mixin _\$${visitor.className} on ${visitor.className} {');
-
-    for (MapEntry<String, DartType> field in visitor.fields.entries) {
-      // buffer.writeln('// next field is called ${field.key} and its type is ${field.value.toString()}');
-
-      // buffer.writeln('@override');
-      // buffer.writeln('${field.value.toString()} get ${field.key} {');
-      // buffer.writeln('  return super.${field.key};');
-      // buffer.writeln('}');
-      // buffer.writeln();
-      buffer.writeln('@override');
-      buffer.writeln('set ${field.key}(${field.value.toString()} value) {');
-      buffer.writeln('  notifyListeners();');
-      buffer.writeln();
-      buffer.writeln('  super.${field.key} = value;');
-      buffer.writeln('}');
-    }
-    buffer.writeln('}');
-
-    return buffer.toString();
-  }
-}
+// import 'package:data_monitor/annotations.dart';
+// import 'package:data_monitor_generators/generators.dart';
+//
+// class DataMonitorGenerator extends GeneratorForAnnotation<DataMonitor> {
+//   @override
+//   String generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) {
+//     final ModelVisitor visitor = ModelVisitor();
+//     final StringBuffer buffer  = StringBuffer();
+//
+//     element.visitChildren(visitor);
+//
+//     buffer.writeln('mixin _\$${visitor.className} on ${visitor.className} {');
+//
+//     for (MapEntry<String, DartType> field in visitor.fields.entries) {
+//       // buffer.writeln('// next field is called ${field.key} and its type is ${field.value.toString()}');
+//
+//       // buffer.writeln('@override');
+//       // buffer.writeln('${field.value.toString()} get ${field.key} {');
+//       // buffer.writeln('  return super.${field.key};');
+//       // buffer.writeln('}');
+//       // buffer.writeln();
+//       buffer.writeln('@override');
+//       buffer.writeln('set ${field.key}(${field.value.toString()} value) {');
+//       buffer.writeln('  notifyListeners();');
+//       buffer.writeln();
+//       buffer.writeln('  super.${field.key} = value;');
+//       buffer.writeln('}');
+//     }
+//     buffer.writeln('}');
+//
+//     return buffer.toString();
+//   }
+// }
--- lib/src/model_visitor.dart
+++ lib/src/model_visitor.dart
@@ -1,25 +1,25 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/dart/element/visitor2.dart';
-import 'package:data_monitor/annotations.dart';
+// import 'package:data_monitor/annotations.dart';
 import 'package:source_gen/source_gen.dart';
 
-class ModelVisitor extends SimpleElementVisitor2<void> {
-  String                className = '';
-  Map<String, DartType> fields    = {};
-
-  @override
-  void visitConstructorElement(ConstructorElement element) {
-    className = element.returnType.toString();
-
-    super.visitConstructorElement(element);
-  }
-
-  @override
-  void visitFieldElement(FieldElement element) {
-    if (TypeChecker.typeNamed(Monitor).hasAnnotationOf(element)) {
-      fields[element.name!] = element.type;
-    }
-    super.visitFieldElement(element);
-  }
-}
+// class ModelVisitor extends SimpleElementVisitor2<void> {
+//   String                className = '';
+//   Map<String, DartType> fields    = {};
+//
+//   @override
+//   void visitConstructorElement(ConstructorElement element) {
+//     className = element.returnType.toString();
+//
+//     super.visitConstructorElement(element);
+//   }
+//
+//   @override
+//   void visitFieldElement(FieldElement element) {
+//     if (TypeChecker.typeNamed(Monitor).hasAnnotationOf(element)) {
+//       fields[element.name!] = element.type;
+//     }
+//     super.visitFieldElement(element);
+//   }
+// }
